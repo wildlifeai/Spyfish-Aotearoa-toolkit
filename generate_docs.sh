2f9846927a3a09727a3e0db4935bcf247a6057d6
Eatkin <edward.atkin@live.com>
2025-03-21 15:03:12 +0000

    Update doc generation to remove references to *.py files (they are based on current users directory structure so no relevance)

--- generate_docs.sh
+++ generate_docs.sh
@@ -9,9 +9,11 @@ find docs -type f -name "*.html" -delete
 package="sftk"
 # Generate the base package documentation and copy to index.html so it can be launched as a http server
 python -m pydoc -w "$package"
+sed -i 's|<a href="[^"]*\.py">[^<]*</a>||g' "$package.html"
 cp -f "$package.html" "docs/index.html"
 # Move the original so linking works
 mv -f "$package.html" "docs/"
+
 
 
 # TODO: This will only work for the first level of modules, need to make it recursive if subdirectories are added
@@ -20,5 +22,6 @@ for module in $package/*.py; do
     file_no_ext="$(basename "$module" .py)"
     python -m pydoc -w "$package.$file_no_ext"
     html_doc="$package.$file_no_ext.html"
+    sed -i 's|<a href="[^"]*\.py">[^<]*</a>||g' "$html_doc"
     mv -f "$html_doc" "docs/"
 done
