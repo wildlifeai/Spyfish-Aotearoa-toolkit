804bb14f47cf81d181957fe244863423cf9383e6
Kalindi Fonda <kalindi.fonda@gmail.com>
2025-03-30 22:22:52 +0200

    code used to generate the csv file with cleaned scientific names

--- sftk/clean_data.py
+++ sftk/clean_data.py
@@ -11,6 +11,7 @@ class ScientificNameEntry:
     scientific_name_to_check: Optional[str] = None
     scientific_names_match: bool = False
     taxon_rank: Optional[str] = None
+    status: str = "unprocessed"
         
 
 class ScientificNameProcessing:
@@ -26,24 +27,28 @@ class ScientificNameProcessing:
 
 
     def clean_name(self):
-        # Genus values are defined as Sp in the DOC data, so dealing with this here.
+        """Cleans scientific_name_to_check and returns the value ready for query or None if invalid."""
+        # Genus values are defined as sp in the DOC data
         if not isinstance(self.scientific_name_to_check, str):
             return None
         
         cleaned_name = self.scientific_name_to_check
-        if self.scientific_name_to_check.endswith("sp"):
+        if self.scientific_name_to_check.lower().endswith(" sp"):
             cleaned_name = cleaned_name.split()[0]
             self.higher_taxon = " sp"
         return cleaned_name
+    
         
 
 
     def query_api(self) -> ScientificNameEntry:
-        """Queries the WoRMS API and returns a ScientificNameEntry populated from the response.""" 
-
+        """Queries the WoRMS API and returns a ScientificNameEntry populated from the response. 
+        
+        API reference: https://www.marinespecies.org/rest/
+        If the resquest is successful it populates all the attributes, if not, only with input info and failure reason.""" 
         cleaned_name = self.clean_name()
         if not cleaned_name:
-            return self.failed_to_process(f"{self.scientific_name_to_check} was not processed, not a valid string.")
+            return self.failed_to_process(f"{self.scientific_name_to_check}, {self.common_name} was not processed, not a valid string.", "invalid name")
 
         # TODO: what if there are multiple species? what does offset 1 do?
         api_url = f"https://www.marinespecies.org/rest/AphiaRecordsByName/{cleaned_name}"
@@ -51,41 +56,46 @@ class ScientificNameProcessing:
         params = { 
             "like": "false",
             "marine_only": "true",
-            "offset": 1
+            "offset": 1 # starts at 1
         }
         try:
             response = requests.get(api_url, params=params)
         except requests.exceptions.RequestException as e:
-            return self.failed_to_process(f"{self.scientific_name_to_check} was not processed, error connecting to API: {e}")
+            return self.failed_to_process(f"{self.scientific_name_to_check}, {self.common_name}  was not processed, error connecting to API: {e}", "api error")
  
         if response.status_code != 200:
-            return self.failed_to_process(f"No results found for scientific name {self.scientific_name_to_check}, API request failed with status code {response.status_code}.")
+            return self.failed_to_process(f"No results found for scientific name {self.scientific_name_to_check}, {self.common_name}  API request failed with status code {response.status_code}.", "api error")
         
         
         try: 
             response_json = response.json()
             if len(response_json) > 1: 
-                logging.warning(f"Multiple results found for {self.scientific_name_to_check}, if that's not expected, check it here: {response_json}")
+                # Currently it selects the first option, WAI, no need to fix yet. 
+                # Example to use to get multiple results: Chrysophrys auratus
+                logging.warning(f"Multiple results found for {self.scientific_name_to_check}, {self.common_name} if that's not expected, check it here: {response_json}")
             response_json = response_json[0]
         except: 
-            return self.failed_to_process(f"No results found for scientific name {self.scientific_name_to_check}, empty API response.")
+            return self.failed_to_process(f"No results found for scientific name {self.scientific_name_to_check}, {self.common_name} empty API response.", "no results")
+        
+        
 
         accepted_scientific_name = response_json.get('valid_name')
         if not accepted_scientific_name:
-            return self.failed_to_process(f"{self.scientific_name_to_check} was not processed, no valid_name found in API response.")
+            return self.failed_to_process(f"{self.scientific_name_to_check}, {self.common_name} was not processed, no valid_name found in API response.", "no accepted name")
 
         return ScientificNameEntry(
             aphia_id=response_json.get("AphiaID"),
             common_name=self.common_name,
             scientific_name=accepted_scientific_name + self.higher_taxon,
-            scientific_name_to_check=cleaned_name,
+            scientific_name_to_check=self.scientific_name_to_check,
             scientific_names_match=accepted_scientific_name == cleaned_name,
             taxon_rank=response_json.get("rank"),
+            status=response_json.get("status")
         )
     
-    def failed_to_process(self, message):
+    def failed_to_process(self, message, status="failure"):
         logging.warning(message)
-        return ScientificNameEntry(common_name=self.common_name, scientific_name_to_check=self.scientific_name_to_check)
+        return ScientificNameEntry(common_name=self.common_name, scientific_name_to_check=self.scientific_name_to_check, status=status)
 
 
 
--- sftk/utils.py
+++ sftk/utils.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def flatten_list(lst: list[list]) -> list:
     """
     Flatten a list of lists.
@@ -13,3 +15,13 @@ def flatten_list(lst: list[list]) -> list:
         item = flatten_list(item) if isinstance(item, list) else [item]
         flattened.extend(item)
     return flattened
+
+
+def read_file_to_df(file_path: str, sheet_name: str = None):
+    # TODO add tests
+    # TODO add failure protection
+    if file_path.endswith(".csv"):
+        return pd.read_csv(file_path)
+    else: 
+        # TODO does it work with None as default? 
+        return pd.read_excel(file_path, sheet_name=sheet_name)
