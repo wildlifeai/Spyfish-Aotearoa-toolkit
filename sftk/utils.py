9a3def0262623fc190f2244ab9fda9cc1b63fb64
Eatkin <edward.atkin@live.com>
2025-04-09 20:03:39 +0100

    Create validation module with abstract base class for validation and sharepoint csv implementation as a child class

--- sftk/utils.py
+++ sftk/utils.py
@@ -1,3 +1,5 @@
+import os
+
 def flatten_list(lst: list[list]) -> list:
     """
     Flatten a list of lists.
@@ -13,3 +15,17 @@ def flatten_list(lst: list[list]) -> list:
         item = flatten_list(item) if isinstance(item, list) else [item]
         flattened.extend(item)
     return flattened
+
+def clamp_n_jobs(n_jobs):
+    """
+    Clamp the number of jobs to a valid range.
+
+    Args:
+        n_jobs (int): The number of jobs to run in parallel.
+
+    Returns:
+        int: The clamped number of jobs.
+    """
+    if n_jobs == -1:
+        return os.cpu_count() or 1
+    return max(1, n_jobs)
