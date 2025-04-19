2245790d0fd7c14e39c4bd9e9bcbabfb073ed39d
Kalindi Fonda <kalindi.fonda@gmail.com>
2025-04-19 14:51:55 +0200

    clean up workflow branch

--- sftk/common.py
+++ sftk/common.py
@@ -1,11 +1,14 @@
 import os
+
 from dotenv import load_dotenv
+
 
 def load_env_wrapper() -> None:
     """
     Wrapper for loading environment variables from a .env file.
 
-    Guard clause added to prevent loading environment variables when running in a GitHub Actions environment.
+    Guard clause added to prevent loading environment variables when running in
+    a GitHub Actions environment.
     """
     if os.getenv("GITHUB_ACTIONS") == "true":
         return
@@ -14,6 +17,9 @@ def load_env_wrapper() -> None:
 
 
 load_env_wrapper()
+
+# General settings
+DEV_MODE = os.getenv("DEV_MODE")
 
 # Email configuration
 EMAIL_USER = os.getenv("EMAIL_USER")
@@ -33,10 +39,25 @@ S3_BUCKET = os.getenv("S3_BUCKET")
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
+# S3 Sharepoint Files
+S3_SHAREPOINT_TEST_CSV = os.getenv("S3_SHAREPOINT_TEST_CSV")
+
+S3_SHAREPOINT_DEFINITIONS_CSV = os.getenv("S3_SHAREPOINT_DEFINITIONS_CSV")
+S3_SHAREPOINT_DEPLOYMENT_CSV = os.getenv("S3_SHAREPOINT_DEPLOYMENT_CSV")
 S3_SHAREPOINT_MOVIE_CSV = os.getenv("S3_SHAREPOINT_MOVIE_CSV")
-S3_SHAREPOINT_SURVEY_CSV = os.getenv("S3_SHAREPOINT_SURVEY_CSV")
+S3_SHAREPOINT_RESERVES_CSV = os.getenv("S3_SHAREPOINT_RESERVES_CSV")
 S3_SHAREPOINT_SITE_CSV = os.getenv("S3_SHAREPOINT_SITE_CSV")
 S3_SHAREPOINT_SPECIES_CSV = os.getenv("S3_SHAREPOINT_SPECIES_CSV")
+S3_SHAREPOINT_SURVEY_CSV = os.getenv("S3_SHAREPOINT_SURVEY_CSV")
+
+# S3 KSO Files
+S3_KSO_TEST_CSV = os.getenv("S3_KSO_TEST_CSV")
+
+S3_KSO_ANNOTATIONS_CSV = os.getenv("S3_KSO_ANNOTATIONS_CSV")
+S3_KSO_MOVIE_CSV = os.getenv("S3_KSO_MOVIE_CSV")
+S3_KSO_SITE_CSV = os.getenv("S3_KSO_SITE_CSV")
+S3_KSO_SPECIES_CSV = os.getenv("S3_KSO_SPECIES_CSV")
+S3_KSO_SURVEY_CSV = os.getenv("S3_KSO_SURVEY_CSV")
 
 # Keywords to monitor in email
 KEYWORDS = [
